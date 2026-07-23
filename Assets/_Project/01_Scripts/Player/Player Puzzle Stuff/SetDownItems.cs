59fc7fd32b4377b446ac7041571a60e4b2612575
Snoopdawg21 <65247774+Snoopdawg21@users.noreply.github.com>
2026-07-23 12:56:23 -0600

    i going to crash out
    what do u mean the reference is null, it shouldn't be getting that far in the code without a fucking reference omggggg

--- Assets/_Project/01_Scripts/Player/Player Puzzle Stuff/SetDownItems.cs
+++ Assets/_Project/01_Scripts/Player/Player Puzzle Stuff/SetDownItems.cs
@@ -3,9 +3,13 @@ using UnityEngine;
 public class SetDownItems : BaseInteractable
 {
     [SerializeField] private PuzzleManager puzzleManager;
+    [SerializeField] private PlayerPuzzleController puzzleController;
     
     public override void Interact()
     {
+        if (!puzzleController.currentlyHeldItem) return;
+        
+        Destroy(puzzleController.currentlyHeldItem);
         puzzleManager.psm.currentPuzzle.UpdateProgress();
     }
 }
