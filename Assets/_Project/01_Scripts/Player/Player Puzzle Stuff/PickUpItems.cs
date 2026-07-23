81813e2736efa74ce6a19ce3a20c7b2294368ee9
Snoopdawg21 <65247774+Snoopdawg21@users.noreply.github.com>
2026-07-22 23:56:46 -0600

    fixed pick up. it be working now
    i set up the interaction system to pick up and put down objects. im gonna work on placing them all into an object next, and having that count up for the kids bedroom puzzle.

--- Assets/_Project/01_Scripts/Player/Player Puzzle Stuff/PickUpItems.cs
+++ Assets/_Project/01_Scripts/Player/Player Puzzle Stuff/PickUpItems.cs
@@ -2,8 +2,10 @@ using UnityEngine;
 
 public class PickUpItems : MonoBehaviour, IInteractable
 {
+    public bool pickUpToggle { get; private set; }
+    
     public void Interact()
     {
-        
+        pickUpToggle = !pickUpToggle;
     }
 }
