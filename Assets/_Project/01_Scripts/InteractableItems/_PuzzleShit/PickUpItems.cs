ef950bfcc01667d229f5f6b87be2be7c307e09c4
Snoopdawg21 <65247774+Snoopdawg21@users.noreply.github.com>
2026-07-24 11:07:09 -0600

    got the kitchen logic working
    The kitchen logic turned out to be stupid easy. ill have to change the destroy to an animation for opening the stuff, but other then that we're cooking chat

--- Assets/_Project/01_Scripts/InteractableItems/_PuzzleShit/PickUpItems.cs
+++ Assets/_Project/01_Scripts/InteractableItems/_PuzzleShit/PickUpItems.cs
@@ -2,24 +2,14 @@ using UnityEngine;
 
 public class PickUpItems : BaseInteractable
 {
-    public int puzzleIndexNum;
-
-    public bool pickUpToggle;
+    public bool pickUpToggle { get; private set; }
     private PlayerPuzzleController puzzleController;
     
     public override void Interact()
     {
         puzzleController = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerPuzzleController>();
         
-        switch (pickUpToggle)
-        {
-            case false:
-                puzzleController.currentlyHeldItem = gameObject;
-                break;
-            case true:
-                puzzleController.currentlyHeldItem  = null;
-                break;        
-        }
         pickUpToggle = !pickUpToggle;
+        puzzleController.currentlyHeldItem = gameObject;
     }
 }
