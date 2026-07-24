6c6582fa300cc027b695e6eec79d774a1f2c9a44
Snoopdawg21 <65247774+Snoopdawg21@users.noreply.github.com>
2026-07-24 13:15:43 -0600

    vhs stuff working
    the vhs stuff is working. for some reason you can't pop out the current vhs if ur not holding one, but its a feature, not a bug ☝️ 😭 i can fuck w it more later, but i feel like im going way to slow w all this shit alr 😭

--- Assets/_Project/01_Scripts/InteractableItems/_PuzzleShit/PickUpItems.cs
+++ Assets/_Project/01_Scripts/InteractableItems/_PuzzleShit/PickUpItems.cs
@@ -3,15 +3,23 @@ using UnityEngine;
 public class PickUpItems : BaseInteractable
 {
     public int puzzleIndexNum;
-    
-    public bool pickUpToggle { get; private set; }
+
+    public bool pickUpToggle;
     private PlayerPuzzleController puzzleController;
     
     public override void Interact()
     {
         puzzleController = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerPuzzleController>();
         
+        switch (pickUpToggle)
+        {
+            case false:
+                puzzleController.currentlyHeldItem = gameObject;
+                break;
+            case true:
+                puzzleController.currentlyHeldItem  = null;
+                break;        
+        }
         pickUpToggle = !pickUpToggle;
-        puzzleController.currentlyHeldItem = gameObject;
     }
 }
