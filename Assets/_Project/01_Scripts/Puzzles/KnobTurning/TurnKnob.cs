182fb953ba58334ef35561526e0d0b3390411899
Snoopdawg21 <65247774+Snoopdawg21@users.noreply.github.com>
2026-07-24 16:45:49 -0600

    knob puzzle finished
    all i have to do is set up a bool on the knobmanager, and then check it if u try to interact w the sink, and water will be set up. (excluding the watering can but dw abt that)

--- Assets/_Project/01_Scripts/Puzzles/KnobTurning/TurnKnob.cs
+++ Assets/_Project/01_Scripts/Puzzles/KnobTurning/TurnKnob.cs
@@ -4,11 +4,17 @@ public class TurnKnob : MonoBehaviour
 {
     [SerializeField] private RectTransform body;
     [SerializeField] private int[] correctPos;
+
+    [SerializeField] private KnobManager knobManager;
+    
+    public bool inRightSpot { get; private set; }
     
     private int rotatePos;
     
     public void OnClick()
     {
+        inRightSpot = false;
+        
         body.Rotate(new Vector3(0, 0, 90), Space.Self);
 
         if (rotatePos < 3)
@@ -22,7 +28,11 @@ public class TurnKnob : MonoBehaviour
 
         foreach (var pos in correctPos)
         {
+            if (pos != rotatePos) continue;
             
+            inRightSpot = true;
+            knobManager.CheckKnobs();
+            break;
         }
     }
 }
