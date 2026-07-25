d875bf80cca0d99fc9696ecb541b6922a1a25768
miss-nibiru <du.herbst@gmail.com>
2026-07-24 18:58:00 -0600

    Add Brick Project Studio assets with Git LFS

---
+++ Assets/Brick Project Studio/Apartment Kit/Scenes/Quitgame.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d205bdfe8363fc94c1faeb80084ad99da2cdce3f9945041ecf6995edee07f66f
+size 488
---
+++ Assets/Brick Project Studio/_BPS Basic Assets/Common/Scripts and Animations/Doors/opencloseStallDoor.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:62b599fd3e2b05a3b11b9120166a4faa527ec31e648560a5ef61e8b72930e048
+size 1170
