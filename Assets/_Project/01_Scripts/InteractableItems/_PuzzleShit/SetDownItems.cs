5a13424312cc408b57ad5ec7e2ffd80070a21ce0
Snoopdawg21 <65247774+Snoopdawg21@users.noreply.github.com>
2026-07-24 21:30:06 -0600

    i got the train puzzle looking nice
    if u ignore removing it from the oven, and fridge, everything looks sick now 😹 doing this made me feel better abt jumping between the diff puzzles earlier tho, cuz that was light work 💪 and now obviously the others will be to. it'll b so fire, we'll b chilling so hard

--- Assets/_Project/01_Scripts/InteractableItems/_PuzzleShit/SetDownItems.cs
+++ Assets/_Project/01_Scripts/InteractableItems/_PuzzleShit/SetDownItems.cs
@@ -6,12 +6,20 @@ public class SetDownItems : BaseInteractable
     [SerializeField] private PlayerPuzzleController puzzleController;
 
     public int puzzleIndexNum;
+
+    public GameObject[] missingPieces;
+
+    private PickUpItems currentItem;
     
     public override void Interact()
     {
         if (!puzzleController.currentlyHeldItem) return;
 
-        if (puzzleController.currentlyHeldItem.GetComponent<PickUpItems>().puzzleIndexNum != puzzleIndexNum) return;
+        currentItem = puzzleController.currentlyHeldItem.GetComponent<PickUpItems>();
+        
+        if (currentItem.puzzleIndexNum < 1 || currentItem.puzzleIndexNum > 6) return;
+
+        missingPieces[currentItem.puzzleIndexNum - 1].SetActive(true);
         
         Destroy(puzzleController.currentlyHeldItem);
         puzzleManager.psm.currentPuzzle.UpdateProgress();
