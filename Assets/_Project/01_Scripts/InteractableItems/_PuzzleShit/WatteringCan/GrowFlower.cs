41a813e657e92cda4fde4edfea8968b9f166ff3b
Snoopdawg21 <65247774+Snoopdawg21@users.noreply.github.com>
2026-07-27 12:25:57 -0600

    THE FLOWERS GROW CHAT!!!
    they grow!! im so happy rn!!!

---
+++ Assets/_Project/01_Scripts/InteractableItems/_PuzzleShit/WatteringCan/GrowFlower.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using UnityEngine;
+
+public class GrowFlower : BaseInteractable
+{
+    [SerializeField] private PlayerPuzzleController puzzleControl;
+    private Vector3 positionIncrease = new Vector3(0, 0.1f, 0);
+    private GameObject flower;
+
+    private WateringCanInteractions can;
+    
+    public override void Interact()
+    {
+        if (!puzzleControl.CurrentlyHeldItem) return;
+        
+        if (!puzzleControl.CurrentlyHeldItem.GetComponent<WateringCanInteractions>()) return;
+        
+        can = puzzleControl.CurrentlyHeldItem.GetComponent<WateringCanInteractions>();
+        if (can.wsm.currentState != can.wsm.fullState) return;
+        can.wsm.SwitchStates(can.wsm.emptyState);
+        flower = gameObject.transform.GetChild(1).gameObject;
+        Debug.Log(flower.name);
+        Growing();
+    }
+
+    private void Growing()
+    {
+        if (flower.transform.position.y < -0.7)
+            StartCoroutine(growTimer());
+    }
+
+    private IEnumerator growTimer()
+    {
+        yield return new WaitForSeconds(0.1f);
+        flower.transform.position += positionIncrease;
+        Growing();
+    }
+}
