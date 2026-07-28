b4ff540ffb83896a5fb5a57af2cfc1018753a5d5
Snoopdawg21 <65247774+Snoopdawg21@users.noreply.github.com>
2026-07-27 23:34:16 -0600

    got the key all hooked up and working
    we up chat, we up 💪

--- Assets/_Project/01_Scripts/InteractableItems/OpenDoor.cs
+++ Assets/_Project/01_Scripts/InteractableItems/OpenDoor.cs
@@ -3,10 +3,23 @@ using UnityEngine;
 public class OpenDoor : BaseInteractable
 {
     [SerializeField] private Animator anim;
+    [SerializeField] private bool needsKey;
+
+    [SerializeField] private PlayerPuzzleController puzzleControl;
     
     public override void Interact() 
     {
-        Debug.Log("hi");
+        if (needsKey)
+        {
+            if (!puzzleControl.CurrentlyHeldItem.GetComponent<GetKey>()) return;
+        }
+        
         anim.SetBool("opening", !anim.GetBool("opening"));
+        needsKey = false;
+
+        if (puzzleControl.CurrentlyHeldItem.GetComponent<GetKey>())
+        {
+            puzzleControl.ConsumeHeldItem();
+        }
     }
 }
