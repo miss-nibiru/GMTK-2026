ea5a30d5c14b93f7672ce0b41638cd80ad228955
miss-nibiru <du.herbst@gmail.com>
2026-07-27 23:35:08 -0600

    Updated oven bug and animation
    # Conflicts:
    #	Assets/TestingHouse/HouseAndDoorPrefabs/HouseWithDoors.prefab

--- Assets/Brick Project Studio/Apartment Kit/Common/Scripts & Animation/OvenOpen/OvenFlip.cs
+++ Assets/Brick Project Studio/Apartment Kit/Common/Scripts & Animation/OvenOpen/OvenFlip.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e071410eef73ce45266f99ddb81a452a87ab1ab82200ef88a6c3bea7a174f30b
+oid sha256:5251b50c87ed630c775c62df30feac8154a71b2deadb9714b24c14b3690149b5
 size 1170
