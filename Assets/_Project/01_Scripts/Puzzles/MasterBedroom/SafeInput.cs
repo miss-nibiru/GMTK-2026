90e6b31a26f59357c9e4f595859099c38f55b26c
miss-nibiru <du.herbst@gmail.com>
2026-08-05 20:18:28 -0600

    Updated safe puzzle

--- Assets/_Project/01_Scripts/Puzzles/MasterBedroom/SafeInput.cs
+++ Assets/_Project/01_Scripts/Puzzles/MasterBedroom/SafeInput.cs
@@ -1,30 +1,20 @@
-using UnityEngine;
 using TMPro;
+using UnityEngine;
 
 public class SafeInput : MonoBehaviour
 {
-    [SerializeField] private SafeManager safeManager;
-    
     [SerializeField] private SafeNumbers numbers;
-    
     [SerializeField] private TextMeshProUGUI currentNum;
     [SerializeField] private int adder;
-    
+
     public void OnClick()
     {
+        
         numbers.currentNum += adder;
-        
-        if (numbers.currentNum < 0)
-        {
-            numbers.currentNum = 9;
-        }
-        else if (numbers.currentNum >= 10)
-        {
-            numbers.currentNum = 0;
-        }
-        
+        if (numbers.currentNum < 0) numbers.currentNum = 9;
+        else if (numbers.currentNum >= 10) numbers.currentNum = 0;
         currentNum.text = numbers.currentNum.ToString();
         
-        safeManager.CheckNumbers();
+        
     }
 }
--- Assets/_Project/01_Scripts/Puzzles/MasterBedroom/SafeManager.cs
+++ Assets/_Project/01_Scripts/Puzzles/MasterBedroom/SafeManager.cs
@@ -25,7 +25,6 @@ public class SafeManager : MonoBehaviour
         if (rightNumbers >= numbers.Length)
         {
             safe.Interact();
-            Destroy(safe.gameObject);
         }
     }
 }
