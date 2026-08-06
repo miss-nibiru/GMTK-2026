eb4bdaaa22000bec5adf56e0d91d53a41be49cd6
miss-nibiru <du.herbst@gmail.com>
2026-08-05 20:36:49 -0600

    Finalize safe update

--- Assets/_Project/01_Scripts/Puzzles/MasterBedroom/SafeManager.cs
+++ Assets/_Project/01_Scripts/Puzzles/MasterBedroom/SafeManager.cs
@@ -2,29 +2,27 @@ using UnityEngine;
 
 public class SafeManager : MonoBehaviour
 {
+    private static readonly int Open = Animator.StringToHash("Open");
     [SerializeField] private SafeNumbers[] numbers;
-
     [SerializeField] private OpenSafe safe;
+    [SerializeField] private Animator animator;
 
-    private int rightNumbers;
+    private int _rightNumbers;
     
     public void CheckNumbers()
     {
-        rightNumbers = 0;
+        _rightNumbers = 0;
         
         foreach (var num in numbers)
         {
-            if (num.targetNum == num.currentNum)
-            {
-                rightNumbers++;
-            }
+            if (num.targetNum == num.currentNum) _rightNumbers++;
         }
-
-        Debug.Log(rightNumbers);
         
-        if (rightNumbers >= numbers.Length)
+        if (_rightNumbers >= numbers.Length)
         {
+            animator.SetTrigger(Open);
             safe.Interact();
         }
+        
     }
 }
